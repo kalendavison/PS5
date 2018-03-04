0d50aceeacb832c07791bae37d141cb5ff65da21
Kalen Davison <davison@wustl.edu>
2018-03-04 13:05:48 -0600

    Package ready for building

--- Development file.R
+++ Development file.R
@@ -6,7 +6,6 @@ setwd("/Users/kalendavison/Desktop/Applied Statistical Programming/GitHub/PS5") 
 
 ## This is run once when the package strcuture is first created
 
-
 ## At this point put the *.R files into the correcto directories and edit the DESCRIPTION file
 
 # Now the NAMESPACE
@@ -24,8 +23,19 @@ getMethod(integrateIt)
 library(integrateIt)
 x<-c(1:10)
 y<-c(11:20)
-integrateIt(x, y, min(x), max(x), "Trapezoid")
-test = integrateIt(x, y, min(x), max(x), "Simpson")
+integrateIt(x, y, 2, 8, "Trapezoid")
+integrateIt(x, y, 2, 8, "Simpson") #return same or similar answers
+test = integrateIt(x, y, 1, 10, "Trapezoid")
+class(test)
 print(test)
 
+current.code <- as.package("integrateIt")
+load_all(current.code) # Load all of the functions so you can use them
+document(current.code) # Make the help files
+check(current.code) # Run the R checks
+install(pkg=current.code, local=TRUE) # Install the package
+getwd()
+build(current.code, path=getwd()) # Build a local copy for you to share
 
+?integrateIt #help file works
+integrateIt(1:10, 2:19, 2, 5, "Trapezoid")
--- integrateIt/R/IntegrateIt.R
+++ integrateIt/R/IntegrateIt.R
@@ -15,7 +15,11 @@
 #' @author Kalen J. Davison
 #' @note The estimate may vary depending on the method of integration chosen
 #' @examples
-
+#' 
+#' myX <- c(1:20) 
+#' myY <- c(21:40) 
+#' integrateIt(myX, myY, 3, 18, "Trapezoid")
+#' integrateIt(myX, myY, 3, 18, "Simpson")
 #' @rdname integrateIt
 #' @aliases integrateIt
 #' @export
@@ -27,6 +31,9 @@ setGeneric(name="integrateIt",
 #' @export
 setMethod("integrateIt",
           definition=function(xvalues, yvalues, start, end, rule){
+            if (length(xvalues) != length(yvalues)){
+              stop("xvalues and yvalues must be of equal length.")
+            }
             xvalues = xvalues[start]:xvalues[end]
             yvalues = yvalues[start]:yvalues[end]
             height = (end - start) / (length(xvalues) - 1)
@@ -43,6 +50,6 @@ setMethod("integrateIt",
               odd_middle = 4*(middle[seq(1,length(middle),2)])
               even_middle = 2*(middle[seq(2,length(middle),2)])
               
-              Simp_area = (h/3) * (sum(first, odd_middle, even_middle, last))
+              Simp_area = (h/3) * (sum(first, sum(odd_middle), sum(even_middle), last))
               return (new("Simpson", xvalues = xvalues, yvalues = yvalues, area = Simp_area)) }
             })
